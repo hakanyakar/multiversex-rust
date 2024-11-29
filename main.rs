0dfc60142873298bc3def82ce5e0fe5da317bcb1
hakanyakar <hakanyakarcomeng@gmail.com>
2024-11-29 22:37:16 +0300

    first

---
+++ main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    let string1 = "Hello, ";
+    let string2 = "world!";
+
+    let concatenated_string = concatenate_strings(string1, string2);
+
+    println!("{}", concatenated_string);
+}
+
+
+fn concatenate_strings(s1: &str, s2: &str) -> String {
+    let mut result = String::new();
+    result.push_str(s1);
+    result.push_str(s2);
+    result
+}
